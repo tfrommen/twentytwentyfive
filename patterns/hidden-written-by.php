ccac9645e6502e20df2f26c36266a93f72e44ab4
Al-Amin Firdows <alaminfirdows@gmail.com>
2024-10-28 23:47:59 +0600

    fix: layout breaks when there are many categories in default single post template

--- patterns/hidden-written-by.php
+++ patterns/hidden-written-by.php
@@ -10,7 +10,7 @@
  */
 
 ?>
-<!-- wp:group {"style":{"spacing":{"blockGap":"0.2em","margin":{"bottom":"var:preset|spacing|60"}}},"textColor":"accent-4","fontSize":"small","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"0.2em","margin":{"bottom":"var:preset|spacing|60"}}},"textColor":"accent-4","fontSize":"small","layout":{"type":"flex","flexWrap":"wrap"}} -->
 <div class="wp-block-group has-accent-4-color has-text-color has-link-color has-small-font-size" style="margin-bottom:var(--wp--preset--spacing--60)">
 	<!-- wp:paragraph -->
 	<p><?php esc_html_e( 'Written by ', 'twentytwentyfive' ); ?></p>
